90418cd7facb18e1f590cdfc53b3950f6e20335c
scivision <scivision@users.noreply.github.com>
2023-08-16 10:47:02 -0400

    use Fortran namelist for clarity/stability

--- src/variables_mod.f90
+++ src/variables_mod.f90
@@ -30,37 +30,38 @@ CONTAINS
 
 subroutine read_globals
 
+	namelist /input_params/ dx, dt, epsilon, sigma, gamma, file_skip, tmax, Nx, Ny, initfile, initname, elecfield
+	!space step
+	!small length scale
+	!surface tension
+	!line tension coeff.
+	!how often to print
+	!how many total time steps to take
+	!system size in the x direction
+	!system size in the y direction
+	!check if it reads the initial condition from a data file (= "yes" or "no")
+	!determining the initial condition
+	!switch to turn on/off the electric field (= "on" or "off")
+
+	namelist /input_elec/ lambda_in, lambda_ex, Km, Kw, Cm, Gm, L, U0
+	!conductivity of interior fluid
+	!conductivity of exterior fluid
+	!capacitance of the lipid membrane
+	!conductance of the lipid membrane
+	!membrane to electrodes length
+	!applied potential
+
 	INTEGER 		  :: num_lines, ios, k
 	character(len=1)  :: junk
 
 	! read initial parameters from file
-	open(1,file='../inputs/input')
-		read(1,*) dx          !space step
-		read(1,*) dt          !time step
-		read(1,*) epsilon     !small length scale
-		read(1,*) sigma		  !surface tension
-		read(1,*) gamma       !line tension coeff.
-		read(1,*) file_skip   ! how often to print
-		read(1,*) tmax        !how many total time steps to take
-		read(1,*) Nx          !system size in the x direction
-		read(1,*) Ny          !system size in the y direction
-		read(1,*) initfile	  !check if it reads the initial condition from a data file (= "yes" or "no")
-		read(1,*) initname    !determining the initial condition
-		read(1,*) elecfield	  !switch to turn on/off the electric field (= "on" or "off")
-	close(1)
+	open(1,file='../inputs/input.nml', status='old')
+	read(1, nml=input_params)
 
 	if (elecfield == 'on') then
-		open(2, file='../inputs/input_elec')
-			read(2,*) lambda_in 	!conductivity of interior fluid
-			read(2,*) lambda_ex		!conductivity of exterior fluid
-			read(2,*) Km
-			read(2,*) Kw
-			read(2,*) Cm			!capacitance of the lipid membrane
-			read(2,*) Gm			!conductance of the lipid membrane
-			read(2,*) L				!membrane to electrodes length
-			read(2,*) U0			!applied potential
-		close(2)
+		read(1, nml=input_elec)
 	endif
+	close(1)
 
 	!initialize specific parameters
 	PSI=0.0d0
@@ -84,8 +85,7 @@ subroutine read_globals
 
 	!test that system sizes do not exceed declared dimensions (avoid this using dynamic allocation)
 	if(Nx+1.gt.Nmax.or.Ny+1.gt.Nmax)then
-		print*, 'One of the system dimensions exceeds array dimensions'
-		stop
+		error stop 'One of the system dimensions exceeds array dimensions'
 	endif
 
 	!teset if the system size given in initial file is the same as it defined in Nx and Ny
@@ -95,21 +95,12 @@ subroutine read_globals
 		do k=1,1000000
 			read(0,*, IOSTAT=ios) junk
 			if (ios /= 0) exit
-			if (k == 1000000) then
-				print*, 'Error: Maximum number of records exceeded'
-				print*, ''
-				print*, 'Runing failure!'
-				STOP
-			endif
+			if (k == 1000000) error stop 'Error: Maximum number of records exceeded'
 			num_lines = num_lines + 1 !number of lines in the file
 		enddo
 		rewind(0)
 		if (mod(num_lines, Nx*Ny) /= 0) then
-			print*, 'Error: The system size of the initial file does not'
-			print*, 'maatch with your domain size Nx and Ny'
-			print*, ''
-			print*, 'Runing failure!'
-			STOP
+			error stop 'Error: The system size of the initial file does not maatch with your domain size Nx and Ny'
 		endif
 	endif
 
